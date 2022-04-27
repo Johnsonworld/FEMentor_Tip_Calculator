59384d244a35d9d4b3716aff73d189f84c675aa5
Johnsonworld <mj49926@gmail.com>
2022-04-26 22:36:43 -0600

    Fix calculator display and some formatting issues.

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-let inputs = document.querySelectorAll("input,button");
-for (let i = 0; i < inputs.length; i++) {
+let inputs = document.querySelectorAll("input,tipPercent");
+
+for (let i = this.tabIndex; i < inputs.length; i++) {
+    
     inputs[i].addEventListener("keypress", function (e) {
+       
         if (e.which == 13 || e.which == 9) {
             e.preventDefault();
+            
             let nextInput = document.querySelectorAll('[tabIndex="' + (this.tabIndex + 1) + '"]');
+            
             if (nextInput.length === 0) {
                 nextInput = document.querySelectorAll('[tabIndex="1"]');
             }
             nextInput[0].focus();
+
         }
     })
 }
 
-function onButtonClick() {
+function onCustomClick() {
+
+
     document.getElementById('customEntry').className = "show";
     document.getElementById('customEntry').focus();
     document.getElementById('customPercent').className = "hide";
     document.getElementById('customPercentLabel').className = "hide";
     document.getElementsByClassName('customPerc').className = "hide";
-}
+    
+    
+    }
+
 
 const checkZero = document.getElementById("peopleCount");
 
@@ -41,7 +52,7 @@ function validate(e) {
         // peopleValue.classList.add("invalid");
         // warning.setAttribute("aria-hidden", false);
         // peopleValue.setAttribute("aria-invalid", true);
-        
+
 
     } else {
 
@@ -59,65 +70,76 @@ function validate(e) {
 }
 
 
-
-
-
-
-
-
-
 function getTipAmount() {
-    
+
     let amount = document.getElementById("bill_amount").value;
     let peoples = document.getElementById("peopleCount").value;
     console.log(typeof peoples);
     if (parseInt(peoples) === 0) {
         return;
     } else {
-    // let selectedPercent = 0;
-    //Get a reference to the form id
-    let theForm = document.forms["calculator"];
-    //Get a reference to the % the user Chooses name="tipPercent":
-    let percentClicked = theForm.elements["tipPercent"];
-    console.log(percentClicked);
-    
-    if (percentClicked.value === "on") {
-        percentChosen = parseInt(document.getElementById("customEntry").value);
-        console.log(percentChosen);
+        // let selectedPercent = 0;
+        //Get a reference to the form id
+        let theForm = document.forms["calculator"];
+        //Get a reference to the % the user Chooses name="tipPercent":
+        let percentClicked = theForm.elements["tipPercent"];
+        console.log(percentClicked);
 
-    } else {
-        percentChosen = parseInt(theForm.elements["tipPercent"].value);
-    }
-   
-    calcTipAmount = ((percentChosen / 100) * parseFloat(amount)) / peoples;
-    total = (calcTipAmount + (parseFloat(amount) / peoples));
-    // let percentChosen = parseInt(theForm.elements["tipPercent"].value);
+        if (percentClicked.value === "on") {
+            percentChosen = parseInt(document.getElementById("customEntry").value);
+            console.log(percentChosen);
+
+        } else {
+            percentChosen = parseInt(theForm.elements["tipPercent"].value);
+        }
+
+        calcTipAmount = ((percentChosen / 100) * parseFloat(amount)) / peoples;
+        total = (calcTipAmount + (parseFloat(amount) / peoples));
+        // let percentChosen = parseInt(theForm.elements["tipPercent"].value);
 
 
-    //Here since there are 4 radio buttons selectedCake.length = 4
-    //We loop through each radio buttons
-    // for (let i = 0; i < percentChosen.length; i++) {
-    //     //if the radio button is checked
-    //     if (percentChosen[i].checked) {
-    //         //we set cakeSizePrice to the value of the selected radio button
-    //         //i.e. if the user choose the 8" cake we set it to 25
-    //         //by using the cake_prices array
-    //         //We get the selected Items value
-    //         //For example cake_prices["Round8".value]"
-    //         selectedPercent = tipAmount[percentChosen[i].value];
-    //         console.log(selectedPercent);
-    //         //If we get a match then we break out of this loop
-    //         //No reason to continue if we get a match
-    //         break;
-    //     }
-    // }
-    //We return the cakeSizePrice
-    
-    
-    document.getElementById("tipCalc").innerHTML = (calcTipAmount).toFixed(2);
-    document.getElementById("totalCalc").innerHTML = (total).toFixed(2);
+        //Here since there are 4 radio buttons selectedCake.length = 4
+        //We loop through each radio buttons
+        // for (let i = 0; i < percentChosen.length; i++) {
+        //     //if the radio button is checked
+        //     if (percentChosen[i].checked) {
+        //         //we set cakeSizePrice to the value of the selected radio button
+        //         //i.e. if the user choose the 8" cake we set it to 25
+        //         //by using the cake_prices array
+        //         //We get the selected Items value
+        //         //For example cake_prices["Round8".value]"
+        //         selectedPercent = tipAmount[percentChosen[i].value];
+        //         console.log(selectedPercent);
+        //         //If we get a match then we break out of this loop
+        //         //No reason to continue if we get a match
+        //         break;
+        //     }
+        // }
+        //We return the cakeSizePrice
+
+
+        document.getElementById("tipCalc").innerHTML = (calcTipAmount).toFixed(2);
+        document.getElementById("totalCalc").innerHTML = (total).toFixed(2);
     }// return calcTipAmount;
 }
 
+function clearForm() {
 
+    document.getElementById("calculator").reset();
+    document.getElementById('customEntry').className = "hide";
+    document.getElementById('customPercent').className = "show";
+    document.getElementById('customPercentLabel').className = "show";
+    document.getElementsByClassName('customPerc').className = "show";
+
+    document.getElementById("inputWarning").classList.remove("visible");
+
+    document.getElementById("peopleLabel").classList.remove("visible");
+
+    document.getElementById("tipCalc").innerHTML = "0.00";
+    document.getElementById("totalCalc").innerHTML = "0.00";
+
+
+
+
+}
 
